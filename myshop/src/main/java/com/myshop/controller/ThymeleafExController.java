ba08aee7ddc0562a3428c1475a502004caddb181
eunmiyo <good3103@gmail.com>
2023-01-11 18:06:05 +0900

    up

---
+++ myshop/src/main/java/com/myshop/controller/ThymeleafExController.java
@@ -0,0 +1,20 @@
+package com.myshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller //컨트롤러의 역할을 하는 클래스를 정의
+@RequestMapping(value="/thymeleaf") //request url경로지정
+public class ThymeleafExController {
+	
+	//http://localhost/thymeleaf/ex01
+	
+	@GetMapping(value="/ex01")
+	public String thymeleafEx01(Model model) {
+		model.addAttribute("data", "타임리프 예제 입니다.");
+		return "thymeleafEx/thymeleafEx01";
+	}
+	
+}
