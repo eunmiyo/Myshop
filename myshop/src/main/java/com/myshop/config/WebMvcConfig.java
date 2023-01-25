57fd45341ae0f06a4acaed9e64c5fadae18a9753
eunmiyo <good3103@gmail.com>
2023-01-25 14:41:29 +0900

    up

--- myshop/src/main/java/com/myshop/config/WebMvcConfig.java
+++ myshop/src/main/java/com/myshop/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	//웹 브라우저에 입력하는 url이 /images로 시작하는 경우 uploadPath에 설정한 폴더를 기준으로 파일을 읽어온다.
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/Images/**")
+		registry.addResourceHandler("/images/**")
 				.addResourceLocations(uploadPath);
 	}
 	
